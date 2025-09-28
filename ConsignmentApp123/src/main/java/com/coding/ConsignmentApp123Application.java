ffe00a431709990053b136a26851220efe6a23e0
suryakanth-dadige <suryakanthdadige@gmail.com>
2025-09-28 16:10:21 +0530

    loading folder #1

---
+++ ConsignmentApp123/src/main/java/com/coding/ConsignmentApp123Application.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsignmentApp123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsignmentApp123Application.class, args);
+	}
+
+}
